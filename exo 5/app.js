0c822af09cc2e55043e777edb57f662f3d2de44b
azxki <cococoursan@gmail.com>
2022-11-17 16:33:04 +0100

    EXO

---
+++ exo 5/app.js
@@ -0,0 +1,7 @@
+let a = prompt("Entrez un premier nombre a virgule :")
+let b = prompt("Entrez un second nombre a virgule :")
+
+a = Math.floor(a);
+a = b * a
+
+alert("Resultat :" + a);
---
+++ exo 6/app.js
@@ -0,0 +1,8 @@
+let a = prompt("Entrez votre âge :");
+
+if (a > 18) {
+    alert("Vous êtes majeur !")
+}
+else if (a < 18){
+        alert("Vous êtes mineur !")
+}
